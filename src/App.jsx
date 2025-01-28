62d76ffcfbe8793283f286011c725485e1fb35f5
shokhsanam18 <shokhsanam18@gmail.com>
2025-01-28 11:55:31 +0500

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Test } from "./pages/Test";
+import { LayoutTest } from "./components/layoutTest";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/Test" element={<Test />} />
+        <Route path="/" element={<LayoutTest />} />
+        <Route path="Test" element={<Test />} />
       </Routes>
     </div>
   );
