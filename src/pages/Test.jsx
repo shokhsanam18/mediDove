5f7e7790505787edaf1e1df2f730baee0c4fe729
shokhsanam18 <shokhsanam18@gmail.com>
2025-01-28 22:31:38 +0500

    feat:nav

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 
 export const Test = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      Test
+      <div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
+  );
 };
