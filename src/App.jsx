2998c97400be6fb155a073b12451fe9a512b2260
Inobat <inobatmirahmedova@gmail.com>
2025-01-31 18:54:44 +0500

    fix:card edited

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,21 @@ function App() {
   return (
     <>
       <Button>hello world</Button>
-      <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-      </Card>
+      <div>
+
+        <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+        </Card>
+      </div>
     </>
   );
 }
