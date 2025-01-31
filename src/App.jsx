43b4b731ccf6c71622e1c2d2992775907d4acb32
Inobat <inobatmirahmedova@gmail.com>
2025-01-31 10:09:45 +0500

    feat:card-template added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
   return (
     <>
       <Button>hello world</Button>
+      <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+      </Card>
     </>
   );
 }
