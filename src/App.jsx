7d2621a9c95d38ec666980118b36dab6238a2ec0
navydandelion <kuvondikovaruhshona@gmail.com>
2025-02-06 22:31:57 +0500

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
-import { Test } from "./pages/Test";
-import { LayoutTest } from "./components/layoutTest";
+import AppointmentPage from "./pages/Appointment";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<LayoutTest />} />
-        <Route path="Test" element={<Test />} />
-      </Routes>
+      <AppointmentPage />
     </div>
   );
 }
---
+++ src/pages/Appointment.jsx
@@ -0,0 +1,12 @@
+function AppointmentPage() {
+  return (
+    <>
+      <div>
+        <img src="./public/img/appoinmentpagephoto.png"></img>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default AppointmentPage;
