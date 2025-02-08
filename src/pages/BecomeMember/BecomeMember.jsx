d8447cb744c33dd5bb5ae7ad42804bdb65135667
navydandelion <kuvondikovaruhshona@gmail.com>
2025-02-08 23:57:52 +0500

    only cards left, everything is done

--- src/pages/BecomeMember/BecomeMember.jsx
+++ src/pages/BecomeMember/BecomeMember.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import AppointmentPage from "./AppointmentSlide";
 import { About } from "@/pages/About/About";
 import FormAppointment from "@/components/ui/FormAppointment";
+import NewsAppointment from "./NewsAppointment";
 export const BecomeMember = () => {
   return (
-    <>
+    <div className="overflow-x-hidden">
       <AppointmentPage />;
-      <span class="block w-full h-px bg-gray-300 shadow-sm shadow-gray-400 opacity-50"></span>
       <About />
-      <span class="block w-full h-px bg-gray-300 shadow-sm shadow-gray-400 opacity-50"></span>
       <FormAppointment />
-    </>
+      <NewsAppointment />
+    </div>
   );
 };
