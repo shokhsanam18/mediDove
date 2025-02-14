f631493cf1dbe9d9815095cca171cc9ae2251e8e
navydandelion <kuvondikovaruhshona@gmail.com>
2025-02-15 01:06:39 +0500

    added form for surgery-types

--- src/pages/Surgery-types/Surgery.jsx
+++ src/pages/Surgery-types/Surgery.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-
+import FormDepartment from "./FormDepartment";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 export const Surgery = () => {
-  return <div>SurgeryIndex</div>;
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <FormDepartment />
+      </QueryClientProvider>
+    </div>
+  );
 };
