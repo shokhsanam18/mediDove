bfb3dd4a3d7037293c815464c1990ecb5d7ee1cf
Inobat <inobatmirahmedova@gmail.com>
2025-02-16 23:40:40 +0500

    feat:news added

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Departments } from "./Departments";
+import CombinedPage from "./News";
 
 export const Index = () => {
 
   return <>
   <Departments></Departments>
+  <CombinedPage></CombinedPage>
   </>;
 };
