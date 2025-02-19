4e47b9ef6bd88409ffc2e90e9d10a0423ac95897
Inobat <inobatmirahmedova@gmail.com>
2025-02-19 22:06:37 +0500

    fix: get consultant fixed

--- src/pages/Index/GetConsultant.jsx
+++ src/pages/Index/GetConsultant.jsx
@@ -5,8 +5,8 @@ import { Plus } from 'lucide-react';
 const GetConsultant = () => {
   return (
     
-    <div
-    className=" relative inset-0 w-full bg-cover bg-no-repeat text-center md:flex justify-between xl:px-32 lg:px-20 sm:px-2 items-center py-[120px] bg-left h-full"
+    <section
+    className=" relative inset-0 w-full bg-cover bg-no-repeat text-center flex sm:px-[5%] px-[2%] py-16 sm:py-24 justify-between items-center  bg-left h-full"
     style={{ backgroundImage: "url('./img/slider-bg-1.jpg')" }}
   >
     <div className="absolute inset-0 bg-[#0d1c27] opacity-90"></div>
@@ -16,8 +16,8 @@ const GetConsultant = () => {
         Trust Us To Be There To Help All & Make Things Well Again.
         </p>
 
-        <Link to='/Blog' className='mt-7'>
-            <button className="flex items-center h-16 w-72 rounded-full focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-[#8fb569] text-white hover:bg-[#e12454] shadow-[0px_0px_5px_#8fb569] hover:shadow-[0px_0px_3px_#e12454] transition-all duration-300 ease-in-out font-semibold ">
+        <Link to='/BecomeMember' className='mt-7'>
+            <button className="flex items-center h-16 w-60 sm:w-72 rounded-full focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-[#8fb569] text-white hover:bg-[#e12454] shadow-[0px_0px_5px_#8fb569] hover:shadow-[0px_0px_3px_#e12454] transition-all duration-300 ease-in-out font-semibold ">
                 <span className="w-14 h-14 flex items-center justify-center bg-white text-black rounded-full border-2 border-white shadow-md ml-2">
                 <Plus size={20}/>
                 </span>
@@ -27,7 +27,7 @@ const GetConsultant = () => {
 
 
     </div>
-  </div>
+  </section>
   )
 }
 
