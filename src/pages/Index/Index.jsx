8dd36427390f056570d04da7eafdb2bf2060c835
Inobat <inobatmirahmedova@gmail.com>
2025-02-20 00:24:06 +0500

    feat: statistics added

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -3,12 +3,14 @@ import { Departments } from "./Departments";
 import CombinedPage from "./News";
 import OurTeam from "./OurTeam";
 import GetConsultant from "./GetConsultant";
+import Statistics from "./Statistics";
 
 export const Index = () => {
 
   return <>
   <Departments></Departments>
   <OurTeam/>
+  <Statistics/>
   <GetConsultant/>
   <CombinedPage></CombinedPage>
   </>;
