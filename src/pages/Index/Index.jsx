5ebd835b10b34b72cd733224e3222d5f947e8fbb
Inobat <inobatmirahmedova@gmail.com>
2025-02-19 21:54:15 +0500

    feat: getconsultant added

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Departments } from "./Departments";
 import CombinedPage from "./News";
 import OurTeam from "./OurTeam";
+import GetConsultant from "./GetConsultant";
 
 export const Index = () => {
 
   return <>
   <Departments></Departments>
   <OurTeam/>
+  <GetConsultant/>
   <CombinedPage></CombinedPage>
   </>;
 };
