ccffdf39fd9843bc287bfbbb598d194ed4d1b58b
Inobat <inobatmirahmedova@gmail.com>
2025-02-20 23:17:34 +0500

    fix: pricing finished

--- src/pages/Index/Pricing.jsx
+++ src/pages/Index/Pricing.jsx
@@ -3,9 +3,11 @@ import { Plus} from "lucide-react";
 import { Link } from "react-router-dom";
 import { Cards_Plans, Cards_Team } from "@/components/ui/Cards";
 import { Cards_Services } from "@/components/ui/Cards";
+import { useState } from "react";
 
 
 const Pricing = () => {
+    const [isYear, setIsYear] = useState(false)
 
     const cardsInfoServices = [
         {
@@ -20,7 +22,7 @@ const Pricing = () => {
           id: 1,
           title: 'Advanced',
           description: 'Ut enim ad minim veniam, quis istomw nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.',
-          content: 'Price: $425.00',
+          content: 'Price: $455.00',
           footer: 'Card Footer',
           src: './img/pricing-thumb-2.png'
         },
@@ -28,7 +30,7 @@ const Pricing = () => {
           id: 2,
           title: 'Advantage',
           description: 'Ut enim ad minim veniam, quis istomw nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.',
-          content: 'Price: $425.00',
+          content: 'Price: $489.00',
           footer: 'Card Footer',
           src: './img/pricing-thumb-3.png'
         },
@@ -44,8 +46,8 @@ const Pricing = () => {
        card_styles: {
          backgroundColor: 'white',
          color: '#223645',
-         padding: '2rem 3rem',
-         gap: '1rem',
+         padding: '3rem 3rem',
+         gap: '2rem',
          textAlign: 'left',
          alignItems: 'start',
          justifyContent: 'center',
@@ -56,8 +58,9 @@ const Pricing = () => {
         margin: '0'
        },
        title_styles: {
-        fontSize: '3rem',
+        fontSize: '2.75rem',
         color: '#223645',
+        fontWeight: '700'
        },
        content_styles: {
         fontWeight: '700',
@@ -66,39 +69,40 @@ const Pricing = () => {
         colorHoverContent: '#73bb1d',
        },
        description_styles: {
-        fontSize: '1rem'
+        fontSize: '1rem',
        }
         
     }
   return (
     <section className="flex w-full flex-col sm:px-[5%] px-[2%] pt-11 pb-16 gap-14 bg-[#F4F9FC]">
       
-        <div>
         <div className="flex items-center justify-between md:flex-row flex-col">
             <div
-            className="md:w-1/2 bg-contain bg-left-top bg-no-repeat sm:w-full"
+            className="md:w-1/2 bg-contain bg-left-top bg-no-repeat w-full sm:w-full"
             style={{ backgroundImage: "url('/img/bgnurse.png')" }}
             >
             <Typography variant="p" className={`font-bold`}>
-            Our Team
+            Our Plans
             </Typography>
             <Typography
                 variant="h2"
-                className="mt-4 text-4xl sm:text-4xl md:text-5xl"
+                className="mt-4 text-4xl sm:text-5xl md:text-6xl"
             >
-                A Professional & Care Provider
+                Pricing & Plans
             </Typography>
             <div className="w-32 my-5 h-1 bg-[linear-gradient(to_right,red_50%,transparent_50%)] bg-[length:15px_100%]"></div>
             </div>
-            <Link to='/Blog' className="hidden md:block">
-            <button className="flex items-center h-16 w-64 rounded-full bg-[#E51E50] text-white font-semibold shadow-lg hover:bg-[#8CB369] transition">
-                <span className="w-14 h-14 flex items-center justify-center bg-white text-black rounded-full border-2 border-white shadow-md ml-2">
-                <Plus size={20}/>
-                </span>
-                <span className="ml-5 text-sm font-semibold tracking-wide">MAKE APPOINTMENT</span>
+            <div className="border-violet-200 border-2 border-opacity-60 rounded-full bg-white">
+            <button className={`text-sm uppercase tracking-wide h-20 w-44 font-bold text-center rounded-full ${isYear ?  'text-[#223645] bg-white' :'bg-[#E51E50] text-white'}  transition-all`}
+                onClick={() => setIsYear(false)}
+            >
+                monthly
             </button>
-            </Link>
-        </div>
+            <button className={`text-sm uppercase tracking-wide h-20 w-44 font-bold text-center rounded-full ${isYear ? 'bg-[#E51E50] text-white' : 'text-[#223645] bg-white' }  transition-all`}
+                onClick={() => setIsYear(true)}>
+                yearly
+            </button>
+            </div>
         </div>
         <Cards_Plans cards={cardsInfoServices} 
                                     cards_styles = {stylesServices.cards_styles}
