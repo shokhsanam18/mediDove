dbddd6afc8823671c36baceca840a71a31f74922
Charos Isroilova <charosbakhtiyarovna001@gmail.com>
2025-02-25 18:46:32 +0500

    Updated part in News Page Links

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -39,7 +39,6 @@ export const News = () => {
           <span style={{ color: "#808e96" }}>23 comments</span>
         </span>,
       ],
-
       footer: "read more ->",
       src: "./img/b1.jpg",
     },
@@ -93,91 +92,8 @@ export const News = () => {
       footer: "read more ->",
       src: "./img/b3.jpg",
     },
-
-    {
-      id: 3,
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adipisic elit, sed do eiusmod tempor incididunt .",
-      description:
-        "There’s a good chance Everyday Health is the most appropriately named blog on this list because it focuses on the health topics that consistently affect a wide range of people. They also get kudos.",
-      content: [
-        <span
-          key="user"
-          style={{ display: "flex", alignItems: "center", gap: "5px" }}
-        >
-          <FontAwesomeIcon icon={faUser} style={{ color: "#e1236b" }} />
-          <span style={{ color: "#808e96" }}>Diboli</span>
-        </span>,
-        <span
-          key="comments"
-          style={{ display: "flex", alignItems: "center", gap: "5px" }}
-        >
-          <FontAwesomeIcon icon={faComment} style={{ color: "#e1236b" }} />
-          <span style={{ color: "#808e96" }}>23 comments</span>
-        </span>,
-      ],
-      footer: "read more ->",
-      // src: "./img/b4.jpg",
-    },
-    {
-      id: 4,
-      title:
-        "This health blog from NPR takes a fairly broad look at the medical world,.",
-      description: "- Rosalina Pong",
-      content: [],
-    },
   ];
-  const stylesNews = {
-    cards_styles: {
-      display: "flex",
-      flexDirection: "column",
-      backgroundColor: "white",
-      justifyContent: "space-around",
-      columnGap: "1.25rem",
-      rowGap: "3rem",
-      padding: "100px 10%",
-      wordSpacing: "0.75rem",
-    },
-    card_styles: {
-      backgroundColor: "#FAF9FF",
-      color: "black",
-      padding: "0 0 50px 0",
-      gap: "1.5rem",
 
-      //  textAlign: 'center',
-      //  alignItems: 'center',
-      //  justifyContent: 'center',
-      //  shadowColor: '#AAC',
-    },
-    img_styles: {
-      width: "100%",
-    },
-    title_styles: {
-      fontSize: "1.28rem",
-      color: "#223645",
-      colorHoverTitle: "#e1236b",
-      padding: "0 50px 0 50px",
-    },
-
-    content_styles: {
-      padding: "0 50px 0 50px",
-
-      fontWeight: "500",
-      color: "#808e96",
-      colorHoverContent: "#e1236b",
-      display: "flex",
-      gap: "20px",
-    },
-
-    footer_styles: {
-      padding: "0 50px 0 50px",
-
-      textTransform: "uppercase",
-      fontWeight: "500",
-      color: "#e1236b",
-      colorHoverFooter: "#73bb1d",
-    },
-  };
   return (
     <>
       <div
@@ -189,78 +105,22 @@ export const News = () => {
           backgroundPosition: "center",
         }}
       >
-        <div className=" flex flex-col md:flex-row items-center text-center  w-full p-40">
+        <div className="flex flex-col md:flex-row items-center text-center w-full p-40">
           <div className="flex flex-col items-center sm:items-start w-full">
-            <Typography variant="h4" className="mb-10  ">
+            <Typography variant="h4" className="mb-10">
               We are here for your care.
             </Typography>
             <Typography variant="h2">News</Typography>
           </div>
-
           <div className="flex flex-row justify-end sm:justify-start mt-1">
             <Typography variant="p" className="cursor-pointer">
-              <Link to="/About">Home</Link>
+              <Link to="/">Home</Link>
             </Typography>
-
             <span className="mx-1">|</span>
             <Typography variant="p" className="text-red-500">
-              News
+              <Link to="/News">News</Link>
             </Typography>
           </div>
-        </div>
-      </div>
-      <div className="flex flex-col sm:flex-row">
-        <div className="sm:w-2/3 w-full">
-          <Cards_News
-            cards={cardsInfoNews}
-            cards_styles={stylesNews.cards_styles}
-            card_styles={stylesNews.card_styles}
-            img_styles={stylesNews.img_styles}
-            shadowColor={stylesNews.card_styles.shadowColor}
-            title_styles={stylesNews.title_styles}
-            colorHoverTitle={stylesNews.title_styles.colorHoverTitle}
-            content_styles={stylesNews.content_styles}
-            colorHoverContent={stylesNews.content_styles.colorHoverContent}
-            footer_styles={stylesNews.footer_styles}
-            colorHoverFooter={stylesNews.footer_styles.colorHoverFooter}
-          ></Cards_News>
-
-          <div className="flex justify-center items-center space-x-3 my-12">
-            <button className="w-12 h-12 flex justify-center items-center rounded-full border border-gray-300 text-gray-500 hover:bg-gray-100 text-lg">
-              «
-            </button>
-
-            {[1, 2, 3].map((page) => (
-              <button
-                key={page}
-                className={`w-12 h-12 flex justify-center items-center rounded-full text-lg font-semibold 
-      ${
-        page === 2
-          ? "bg-red-500 text-white"
-          : "border border-gray-300 text-gray-500 hover:bg-gray-100"
-      }`}
-              >
-                {page < 10 ? `0${page}` : page}
-              </button>
-            ))}
-
-            <span className="w-12 h-12 flex justify-center items-center rounded-full border border-gray-300 text-gray-500 text-lg">
-              ...
-            </span>
-
-            <button className="w-12 h-12 flex justify-center items-center rounded-full border border-gray-300 text-gray-500 hover:bg-gray-100 text-lg">
-              »
-            </button>
-          </div>
-        </div>
-        <div className="sm:w-1/3 w-full p-24 pl-0 flex flex-col items-center gap-y-10">
-          <NewsCardOne />
-          <NewsCardTwo />
-          <NewsCardThree />
-          <NewsCardFour />
-          <NewsCardFive />
-          <NewsCardSix />
-          <NewsCardSeven />
         </div>
       </div>
     </>
